cca5dd1589904c2f650bd9f01496b0460883d3d4
Гончаров Даниил <dgoncharov@rnds.pro>
2025-12-26 08:27:18 +0300

    update nvim

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 alias vim="/usr/bin/nvim"
-alias ssh="kitty +kitten ssh"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+if set -q KITTY_PID
+    alias ssh="kitty +kitten ssh"
+end
 if test -n "$XDG_RUNTIME_DIR" -a -S "$XDG_RUNTIME_DIR/ssh-agent.socket"
     set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 end
--- nvim/lua/plugins/base.lua
+++ nvim/lua/plugins/base.lua
@@ -3,8 +3,17 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 Plug 'sheerun/vim-polyglot'
-Plug 'morhetz/gruvbox'
+Plug 'fneu/breezy'
+Plug 'itchyny/lightline.vim'
 vim.call('plug#end')
 
-vim.cmd("colorscheme gruvbox") 
-
+-- Breezy цветовая схема
+vim.opt.termguicolors = true
+vim.env.COLORTERM = "truecolor"
+vim.opt.background = "dark"
+vim.cmd("colorscheme breezy")
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+-- Lightline Breezy
+vim.g.lightline = {
+  colorscheme = "breezy"
+}
